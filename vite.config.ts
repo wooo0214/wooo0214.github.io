e4e2c1fd2d07f6d90200e50cc2366eb709c3a798
wooo <435705052@qq.com>
2026-08-16 18:27:16 +0800

    fix Pages root asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // This is the root personal site: https://<username>.github.io/
+  base: '/',
 })
